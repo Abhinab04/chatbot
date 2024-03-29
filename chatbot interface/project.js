9a3fcf02dd9b3ff635ddf6102e5235f36848eced
ABHINAB SHARMA <146541054+Abhinab04@users.noreply.github.com>
2024-03-29 21:05:51 +0530

    Update project.js

--- chatbot interface/project.js
+++ chatbot interface/project.js
@@ -1,4 +1,13 @@
-document.getElementById("btn1").addEventListener('click',function(){
-   alert("start speaking");
-    //deploy your code here....
-});
+function getResponse(){
+    alert("Get your Response via Cleopatra");
+    outputTextArea.value="";
+    var client=new XMLHttpRequest();
+    client.open("Get",'/get_response',true);
+    client.onreadystatechange=() =>{
+        if(client.readyState==3 && client.status==200){
+          outputTextArea.value+=client.responseText;
+          outputTextArea.scrollTop=outputTextArea.scrollHeight;
+        }
+    };
+    client.send();
+}
