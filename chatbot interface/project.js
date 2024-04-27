5257b2a8551b1aa79a5df31ac4a50610de2f8e53
ABHINAB SHARMA <146541054+Abhinab04@users.noreply.github.com>
2024-04-27 16:03:10 +0530

    Update project.js

--- chatbot interface/project.js
+++ chatbot interface/project.js
@@ -1,13 +1,30 @@
 function getResponse(){
     alert("Get your Response via Cleopatra");
-    outputTextArea.value="";
-    var client=new XMLHttpRequest();
-    client.open("Get",'/get_response',true);
-    client.onreadystatechange=() =>{
-        if(client.readyState==3 && client.status==200){
+    const log=document.querySelector(".event-log-contents");
+    const reload=document.querySelector("#reload");
+    reload.addEventListener("click",()=>{
+      log.textContent="";
+      outputTextArea.value="";
+      var client=new XMLHttpRequest();
+      client.open("Get",'/get_response',true);
+      client.onreadystatechange=() =>{
+       if(client.readyState==3 && client.status==200){
           outputTextArea.value+=client.responseText;
           outputTextArea.scrollTop=outputTextArea.scrollHeight;
         }
-    };
+      };
     client.send();
+      setTimeout(()=>{
+        window.location.reload(true);
+      },200);
+    });
+    window.addEventListener("load",(Event)=>{
+      log.textContent+="load\n";
+    });
+    document.addEventListener("readystatechange",(Event)=>{
+      log.textContent+=`readystate :${document.readyState}\n`;
+    });
+    document.addEventListener("DOMContentLoaded",(Event)=>{
+      log.textContent+="DOMContentLoaded\n";
+    });
 }
