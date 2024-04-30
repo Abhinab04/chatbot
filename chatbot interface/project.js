a4a7f6fb6805d9208f86910ef46fa1da7855b196
ABHINAB SHARMA <146541054+Abhinab04@users.noreply.github.com>
2024-04-30 21:34:49 +0530

    Update project.js

--- chatbot interface/project.js
+++ chatbot interface/project.js
@@ -1,30 +1,20 @@
-function getResponse(){
-    alert("Get your Response via Cleopatra");
-    const log=document.querySelector(".event-log-contents");
-    const reload=document.querySelector("#reload");
-    reload.addEventListener("click",()=>{
-      log.textContent="";
-      outputTextArea.value="";
-      var client=new XMLHttpRequest();
-      client.open("Get",'/get_response',true);
-      client.onreadystatechange=() =>{
-       if(client.readyState==3 && client.status==200){
-          outputTextArea.value+=client.responseText;
-          outputTextArea.scrollTop=outputTextArea.scrollHeight;
-        }
-      };
-    client.send();
-      setTimeout(()=>{
-        window.location.reload(true);
-      },200);
+document.addEventListener("DOMContentLoaded",()=>{
+   const reload=document.getElementById("reload");
+   reload.addEventListener('click',function(){
+    fetch('/getResponse',{
+        method:'POST',
+        headers:{
+            "Content-type":'application/x-www-form-urlencoded'
+        },
+        body:'input'
+    })
+    .then(Response=>Response.json)
+    .then(data =>{
+        const responseid=document.getElementById('output');
+        responseid.value=data.Response;
+    })
+    .catch(error =>{
+        console.error("error",error);
     });
-    window.addEventListener("load",(Event)=>{
-      log.textContent+="load\n";
-    });
-    document.addEventListener("readystatechange",(Event)=>{
-      log.textContent+=`readystate :${document.readyState}\n`;
-    });
-    document.addEventListener("DOMContentLoaded",(Event)=>{
-      log.textContent+="DOMContentLoaded\n";
-    });
-}
+   });
+});
