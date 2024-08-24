cf276b82f9b6c263c1a2d02fbf2b09582ac8ada7
x_Bastille <148121344+XBastille@users.noreply.github.com>
2024-08-24 18:08:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 pip install waitress
+pip install SpeechRecognition pyttsx3 google-generativeai selenium requests beautifulsoup4 win10toast Flask
 waitress-serve --port=$PORT flask_try:app
